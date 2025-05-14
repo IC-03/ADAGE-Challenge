416c1c718bf293abe710700667c578bb00cb37d6
pauzca <paula.uzcategui68@gmail.com>
2025-05-14 09:05:44 -0500

    reduce output

--- run_spark.py
+++ run_spark.py
@@ -3,7 +3,42 @@ from pyspark.sql import SparkSession
 import numpy as np
 import random
 import argparse
-from accumulators import MatrixAccumulatorWrapper
+from pyspark.accumulators import AccumulatorParam
+
+#################### clases #################################
+# Clase acumuladora (usando listas de listas)
+class MatrixAccumulatorParam(AccumulatorParam):
+    def zero(self, value):
+        rows = len(value)
+        cols = len(value[0])
+        return [[0.0 for _ in range(cols)] for _ in range(rows)]
+
+    def addInPlace(self, val1, val2):
+        for i in range(len(val1)):
+            for j in range(len(val1[0])):
+                val1[i][j] += val2[i][j]
+        return val1
+
+# Wrapper de la clase acumuladora
+class MatrixAccumulatorWrapper:
+    def __init__(self, sc, shape):
+        self.shape = shape
+        self.accumulator = sc.accumulator(
+            [[0.0 for _ in range(shape[1])] for _ in range(shape[0])],
+            MatrixAccumulatorParam()
+        )
+
+    def addRow(self, row_index, row_contribution):
+        temp = [[0.0 for _ in range(self.shape[1])] for _ in range(self.shape[0])]
+        temp[row_index] = list(row_contribution)
+        self.accumulator.add(temp)
+
+    def add(self, matrix):
+        self.accumulator.add(matrix)
+
+    def get(self):
+        return self.accumulator.value
+
 
 ################# funciones ##########################
 # Calcular la media de cada columna
@@ -185,7 +220,8 @@ def main(sc):
 
 
     # Columns of C are the principal components
-    sc.parallelize(C).map(lambda row: ",".join(map(str, row))).saveAsTextFile(args.output)
+    sc.parallelize(C).map(lambda row: ",".join(map(str, row))).coalesce(1).saveAsTextFile(args.output)
+
 
 
 main(sc)
